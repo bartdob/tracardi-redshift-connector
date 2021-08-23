9d1f629a22db4aaee7f4202227c8319d12ad6236
Bartosz Dobrosielski <dobry1@pm.me>
2021-08-23 14:45:08 +0200

    connect to database

--- tracardi_redshift_connector/plugin/redshift_connector.py
+++ tracardi_redshift_connector/plugin/redshift_connector.py
@@ -1,24 +1,61 @@
 import psycopg2
+import pprint
 
 
 def redshift_conn(*args, ** kwargs):
     print("start connection")
 
-    # config = kwargs['config']
+    # configuration = {'dbname': 'database_name',
+    #                  'user': 'user_name',
+    #                  'pwd': 'user_password',
+    #                  'host': 'redshift_endpoint',
+    #                  'port': 'redshift_password'
+    #                  }
+
     try:
         conn = psycopg2.connect(dbname='dev',
-                                host='172.31.4.237', #ipadres
+                                host='3.123.191.145', #ipadres
                                 port=5439,
                                 user='awsuser',
-                                password='**********')
-        # conn = psycopg2.connect(dbname=config['dbname'],
-        #                         host=config['host'],
-        #                         port=config['port'],
-        #                         user=config['user'],
-        #                         password=config['password'])
+                                password='Kaloryfer1')
+
         print("SUCCESS")
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema';")
+        rows = cur.fetchall()
+        for row in rows:
+            print (row)
+        cur.close()
+        conn.close()
+        print("connection close")
+
     except Exception as err:
         print("Error:", err)
         conn = None
         print("end conn ")
     return conn
+
+
+# def select(*args, **kwargs):
+#     cur = kwargs['cur']
+#
+#     try:
+#         cur.execute("SELECT * FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema';")
+#     except Exception as err:
+#             print (err.code,err)
+#
+#
+#     rows = cur.fetchall()
+#     for row in rows:
+#         print (row)
+#
+#
+# cursor = conn.cursor()
+# print ('start select')
+# select(cur=cursor)
+# print ('finish')
+#
+# cursor.close()
+# for n in conn.notices():
+#     pprint(n)
+# conn.close()
